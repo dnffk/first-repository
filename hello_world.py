358292119d436313da18ecde9a2fb5a2afb9754d
dnffk <wndus7188@naver.com>
2023-09-14 18:56:09 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,6 @@
 def print_hello():
-    print("Hello World!!")
+    print("My name is Park Ju Yeon! Nice to meet you!")
+    print("Hello Github!! Hello world!!")
 
 if __name__ == '__main__':
     print_hello()
